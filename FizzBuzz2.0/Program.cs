041d7b0f1827c5ac32d68c62f90ba5abf54537fc
hancxxck <hancockc8@hotmail.co.uk>
2022-02-01 14:51:17 +0000

    Fixing stuff

--- FizzBuzz2.0/Program.cs
+++ FizzBuzz2.0/Program.cs
@@ -8,31 +8,43 @@ namespace FizzBuzz2._0
         {
             
             {
-                string fizz = "Fizz";
-                string buzz = "Buzz";
-                int j = 0; 
+                int j = 0;
                 for (int i = 1; i <= 100; i++)
                 {
-                   
+                    
                     string results = "";
                     if (i % 3 == 0 && i % 5 == 0)
-                    { Console.WriteLine("FizzBuzz"); }
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
 
-                    if (i % 3 == 0)
-                    { Console.WriteLine(fizz); }
+                    else if (i % 3 == 0)
+                    { 
+                        Console.WriteLine("Fizz");
+                    }
 
-                    if (i % 5 == 0)
-                    { Console.WriteLine(buzz); }
-                    
-                    while (j > 0)
-                       if (j % 10 == 3)
-                            { Console.WriteLine(fizz); }
-                       if (j % 10 == 5)
-                            { Console.WriteLine(buzz); }
-
+                    else if (i % 5 == 0)
+                    { 
+                        Console.WriteLine("Buzz");
+                    }
+                   
 
                     else Console.WriteLine(i);
                     if (results.Length == 0) results = i.ToString();
+
+                    bool fizz;
+                    bool buzz;
+                    while (j > 0)
+                        if (j % 10 == 3)
+                        {
+                            fizz = true;
+                            Console.WriteLine("Fizz");
+                        }
+                        if (j % 10 == 5)
+                        {
+                        buzz = true;
+                        Console.WriteLine("Buzz");
+                        }
                 }
             }
   
