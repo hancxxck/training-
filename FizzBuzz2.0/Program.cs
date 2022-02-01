8b9e560ae3fc8257d6bfda7db53c11e6ba3eb3ef
hancxxck <hancockc8@hotmail.co.uk>
2022-02-01 13:19:05 +0000

    Add project files.

---
+++ FizzBuzz2.0/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                int j = 0; 
+                for (int i = 1; i <= 100; i++)
+                {
+                    string results = "";
+                    if (i % 3 == 0 && i % 5 == 0)
+                    { Console.WriteLine("FizzBuzz"); }
+
+                    if (i % 3 == 0)
+                    { Console.WriteLine("Fizz"); }
+
+                    if (i % 5 == 0)
+                    { Console.WriteLine("Buzz"); }
+
+                    while (j > 0) 
+                        if (j % 10 == 3)
+                        { Console.WriteLine("Fizz"); }
+
+
+                        else Console.WriteLine(i);
+                    if (results.Length == 0) results = i.ToString();
+                }
+            }
+  
+        }      
+       
+    }
+}
